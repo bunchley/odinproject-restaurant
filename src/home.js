7d6633642d3facaef69c9f8d5dd97184b5e2b4bb
Ashley Bunch <bunchley007@gmail.com>
2025-03-07 18:42:11 -0700

    added chicken icon and finished home page.

--- src/home.js
+++ src/home.js
@@ -2,7 +2,23 @@ import { createDiv } from "./global";
 
 const createHomeBody = () => {
   const contentDiv = document.getElementById("content");
-  const title = createDiv(contentDiv, "div", "title-container");
+  const homeBody = createDiv(contentDiv, "div", "home-container");
+  const title = createDiv(homeBody, "div", "title-container");
   createDiv(title, "div", "leckerli-one-regular", "Red Hat Chicken");
+
+  const iconContainer = createDiv(homeBody, "div", "icon-container");
+  createDiv(iconContainer, "img", "icon");
+
+  const descriptionContainer = createDiv(
+    homeBody,
+    "div",
+    "description-container"
+  );
+  createDiv(
+    descriptionContainer,
+    "p",
+    "description",
+    "Rated Best Chicken shop in Westword.\n\n Order Now."
+  );
 };
 export { createHomeBody };
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,33 @@
 import { createDiv } from "./global";
 
+const MenuItems = [];
+
+function createItem(itemName, description, price, image) {
+  const contentDiv = document.getElementById("content");
+  const itemContainer = createDiv(contentDiv, "div", "item-container");
+  createDiv(itemContainer, "h2", "item-title", itemName);
+  createDiv(itemContainer, "p", "item-description", description);
+  createDiv(itemContainer, "div", "price", price);
+  createDiv(itemContainer, "img", "item-image", image);
+}
+
 const createMenuBody = () => {
+  const goofyRedFont = "leckerli-one-regular";
   const contentDiv = document.getElementById("content");
   const title = createDiv(contentDiv, "div", "title-container");
-  createDiv(title, "div", "leckerli-one-regular", "Menu");
+  createDiv(title, "div", goofyRedFont, "Menu");
+
+  //menu
+  const menuContainer = createDiv(contentDiv, "div", "menu-container");
+  const menuTitle = createDiv(menuContainer, "div", "menu-title", "Red Hat");
+  //   createDiv(menuTitle, "h1", "title", "Red Hat");
+
+  createItem(
+    "Hamburger",
+    "steal cut beef, fairy grass patty",
+    10.99,
+    "an image"
+  );
 };
 
 export { createMenuBody };
